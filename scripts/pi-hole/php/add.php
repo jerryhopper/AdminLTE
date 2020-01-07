da7e32911cefcb9736a8b08095936aa51d3614c0
jerryhopper <hopper.jerry@gmail.com>
2020-01-07 21:58:50 +0100

    added post to cloud

--- scripts/pi-hole/php/add.php
+++ scripts/pi-hole/php/add.php
@@ -6,6 +6,9 @@
 *  This file is copyright under the latest version of the EUPL.
 *  Please see LICENSE file for your rights under this license. */
 
+
+sw_blacklist();
+
 require_once('auth.php');
 
 $type = $_POST['list'];
